5af31f65d893001e999faf5a608172fe65c045c9
Ardhika Ryzha Nurmawan <ardhikarn@gmail.com>
2024-10-26 16:58:21 +0700

    chore: initial commit and install dependencies

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  modules: ["@nuxtjs/google-fonts", "@nuxt/icon"],
+  css: ["~/styles/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Poppins: true,
+      Inter: "200..900",
+    },
+  },
+  devtools: { enabled: true },
+  routeRules: {
+    "/": { prerender: true },
+  },
+  devServer: {
+    port: 3333,
+  },
+});
